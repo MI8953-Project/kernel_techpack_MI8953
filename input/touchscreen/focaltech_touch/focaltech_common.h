4262f5df2d8d9342c552a15118fe23a90512d0fe
RenzAlt <129452476+shenprjkt@users.noreply.github.com>
2024-11-03 05:03:38 +0000

    mi8953: ts: fts: Kang FTS_CHIP_TYPE_MAPPING from msm-4.9 FTS driver

--- input/touchscreen/focaltech_touch/focaltech_common.h
+++ input/touchscreen/focaltech_touch/focaltech_common.h
@@ -54,6 +54,8 @@
 #define FTS_CHIP_IDC(type)            (((type) & FLAGBIT(FLAG_IDC_BIT)) == FLAGBIT(FLAG_IDC_BIT))
 #define FTS_HID_SUPPORTTED(type)      (((type) & FLAGBIT(FLAG_HID_BIT)) == FLAGBIT(FLAG_HID_BIT))
 
+#define FTS_CHIP_TYPE_MAPPING {{0x02, 0x54, 0x22, 0x54, 0x22, 0x00, 0x00, 0x54, 0x2C}}
+
 #define FILE_NAME_LENGTH                    128
 #define ENABLE                              1
 #define DISABLE                             0
